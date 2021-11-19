b17de47ce75db156fba794e3081fba0377bc39d4
jeffmayn <jeff@gyldenbrand.dk>
2021-11-19 22:12:05 +0100

    fix

--- PrintServer/src/main/java/logic/Database.java
+++ PrintServer/src/main/java/logic/Database.java
@@ -112,9 +112,7 @@ public class Database {
 				  c = DriverManager.getConnection("jdbc:sqlite:database\\database.db");
 			     } else {
 			    	 System.out.println("Database does not exists. See Database.java line 115");
-			    	 // if database file does not exists, uncomment this to create a new database 
-			    	 //
-			    	 /*
+
 			    	 c = DriverManager.getConnection("jdbc:sqlite:database\\database.db");
 
 			    	 if(tableExists("users")){
@@ -135,7 +133,7 @@ public class Database {
 							// populates the database									
 							dummyData();
 			    		}	
-			    	 */
+			    	 
 			     }
 		} catch ( Exception e ) {
 			System.err.println("[Server]: " + e.getClass().getName() + " --> " + e.getMessage() );
